eeeeee05eec8678481fb34772612931caef66ca5
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-09 15:52:35 -0300

    feat: iniciei a criação de funções de consultas no DB

---
+++ scripts/src/utils/createSchedule.js
@@ -0,0 +1,5 @@
+import * as SQLite from 'expo-sqlite';
+
+export default function createSchedule() {
+    // código...
+}
---
+++ scripts/src/utils/getSchedules.js
@@ -0,0 +1,5 @@
+import * as SQLite from 'expo-sqlite';
+
+export default function getSchedules() {
+    // código...
+}
