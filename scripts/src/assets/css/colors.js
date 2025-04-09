da244fcd58eb7180b15c222c42491ded8a5bf2a9
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-09 15:51:19 -0300

    feat: arquivo só com cores

---
+++ scripts/src/assets/css/colors.js
@@ -0,0 +1,17 @@
+const theHeapstersPalete = {
+    color1: "#725EF2",
+    color2: "#5D4EBF",
+    color3: "#403873",
+    color4: "#D9D9D9",
+    color5: "#262626"
+}
+
+const projectPalete = {
+    color1: "#C0933E",
+    color2: "#F4ECDD",
+    color3: "#644C20",
+    color4: "#7C7471",
+    color5: "#C0C0B7"
+}
+
+export { theHeapstersPalete, projectPalete };
