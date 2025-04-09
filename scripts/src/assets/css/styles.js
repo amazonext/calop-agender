bfd782815a76a300c1e5fd7918451302b30a6f72
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-09 15:51:52 -0300

    mov: mudei o nome da pasta "styles" para "css"

---
+++ scripts/src/assets/css/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { projectPalete } from './colors';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: projectPalete.color5
+    }
+});
