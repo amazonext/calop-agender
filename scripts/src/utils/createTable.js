881f75a5cf0a28c0563b83dfb372049783b51f7a
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-10 02:28:50 -0300

    feat: criei uma função que inicializa a tabela do DB

---
+++ scripts/src/utils/createTable.js
@@ -0,0 +1,30 @@
+import * as SQLite from 'expo-sqlite';
+
+export default async function createTable() {
+    try {
+        const db = await SQLite.openDatabaseAsync("./db/my-db.db");
+
+        const result = await db.getAllAsync(`
+            SELECT name FROM sqlite_master
+            WHERE type='table' AND name='schedules';
+        `);
+
+        if (result.length > 0) {
+            console.log("Tabela 'schedules' já existe. Nenhuma ação foi tomada.");
+        } else {
+            await db.execAsync(`
+                CREATE TABLE schedules (
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    procediments TEXT NOT NULL,
+                    hour TEXT NOT NULL,
+                    value INTEGER NOT NULL
+                );
+            `);
+            console.log("Tabela 'schedules' foi criada com sucesso!");
+        }
+
+        return db;
+    } catch (error) {
+        console.error("Erro ao verificar/criar tabela: ", error);
+    }
+}
