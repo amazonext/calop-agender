8e5b154014b90fc96bd331ca229653d2f4d8c1ce
Pedro Arthur <freirepedroarthur@gmail.com>
2025-04-15 00:21:14 -0300

    feat: adicionei o código de edição de agendamentos

--- scripts/src/utils/editSchedule.js
+++ scripts/src/utils/editSchedule.js
@@ -1,11 +1,21 @@
 import * as SQLite from 'expo-sqlite';
 import createTable from './createTable';
 
-export default function ediSchedule() {
-    // código...
+export default async function ediSchedule(id, procediments, hour, value) {
     try {
-        createTable();
-    } catch (error) {
+        const db = await createTable();
 
+        await db.runAsync(
+            `UPDATE schedules
+             SET procediments = ?, 
+             hour = ?, 
+             value = ? 
+             WHERE id = ?`,
+            [procediments, hour, value, id]
+        );
+
+        console.log("Agendamento enviado com sucesso!");
+    } catch (error) {
+        console.error("Erro ao enviar agendamento: ", error);
     }
 }
