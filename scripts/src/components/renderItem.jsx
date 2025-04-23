26891738c6cda6ecfce4d8cbe406c88aafc613c7
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-23 20:28:59 -0300

    feat: criei um modelo de slide

---
+++ scripts/src/components/renderItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import styles from '../assets/css/styles';
+
+export default renderItem = ({ item }) => (
+    <View style={{... styles.slide, backgroundColor: item.backgroundColor }}>
+        <Image style={styles.image} source={item.image} />
+        <Text style={{... styles.titleSlide, color: item.colors.title }}>{item.title}</Text>
+        <Text style={{ ...styles.text, color: item.colors.text }}>{item.text}</Text>
+    </View>
+);
