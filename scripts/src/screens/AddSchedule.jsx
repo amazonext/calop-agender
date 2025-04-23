88e4c0d482ec84b6a4fb682de2b294f6dc57c39b
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-23 20:33:39 -0300

    feat: tela de criação de modelos de agendamento

--- scripts/src/screens/AddSchedule.jsx
+++ scripts/src/screens/AddSchedule.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, Button } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 // styles
@@ -7,58 +7,53 @@ import styles from '../assets/css/styles'
 
 export default function AddSchedule() {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.content}>
-                <View>
-                    <Text style={styles.titleItem}>Novo item</Text>
+        <SafeAreaView style={{ ...styles.container, gap: 30 }}>
+            <View>
+                <Text style={styles.titleItem}>Novo item</Text>
+            </View>
+
+            <View style={{ ...styles.content, gap: 20, padding: 20 }}>
+                <View style={styles.field}>
+                    <Text style={styles.label}>Procedimento</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite o procedimento"
+                        placeholderTextColor="#fff"
+                    />
                 </View>
 
-                <View style={{ gap: 10 }}>
-                    {/* campos pra preencher */}
-                    <View style={styles.field}>
-                        <Text style={styles.label}>Procedimento</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Digite o procedimento"
-                            placeholderTextColor="#fff"
-                        />
-                    </View>
-
-                    <View style={styles.field}>
-                        <Text style={styles.label}>Valor</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Digite o valor"
-                            placeholderTextColor="#fff"
-                        />
-                    </View>
-
-                    <View style={styles.field}>
-                        <Text style={styles.label}>Detalhamento (Opcional)</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Digite o procedimento"
-                            placeholderTextColor="#fff"
-                        />
-                    </View>
-
-                    <View style={styles.field}>
-                        <Text style={styles.label}>Profissional</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Digite o nome do profissional"
-                            placeholderTextColor="#fff"
-                        />
-                    </View>
+                <View style={styles.field}>
+                    <Text style={styles.label}>Valor</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite o valor"
+                        placeholderTextColor="#fff"
+                    />
                 </View>
 
-                <View style={styles.Button}>
-                    {/* botão de enviar */}
-                    <TouchableOpacity>
-                        <Text style={styles.Button}>Criar item</Text>
-                    </TouchableOpacity>
+                <View style={styles.field}>
+                    <Text style={styles.label}>Detalhamento (Opcional)</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite o procedimento"
+                        placeholderTextColor="#fff"
+                    />
+                </View>
+
+                <View style={styles.field}>
+                    <Text style={styles.label}>Profissional</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite o nome do profissional"
+                        placeholderTextColor="#fff"
+                    />
                 </View>
             </View>
+
+            <Button
+                title="Criar item"
+                color="#D6AE4F"
+            />
         </SafeAreaView>
     )
 }
