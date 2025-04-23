1c3a13cb1e611b4e1db180091f4cf0b779f0bd33
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-04-23 20:33:59 -0300

    feat: navegação entre telas

---
+++ scripts/src/navigation/MainTabs.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { Image, TouchableOpacity } from 'react-native';
+
+// Telas
+import Home from '../screens/Home';
+import AddSchedule from '../screens/AddSchedule';
+import MySchedules from '../screens/MySchedules';
+import Register from '../screens/Register';
+
+// Cores
+import { projectPalete } from '../assets/css/colors';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainTabs({ navigation }) {
+    return (
+        <Tab.Navigator
+            initialRouteName="Tela inicial"
+            screenOptions={({ route }) => ({
+                headerTitle: "Calop Agender",
+                headerTintColor: projectPalete.color3,
+                headerStyle: { backgroundColor: projectPalete.color1 },
+                headerLeft: () => (
+                    <Image
+                        source={require('../assets/images/logo-app.png')}
+                        style={{ width: 30, height: 30, marginLeft: 10 }}
+                    />
+                ),
+                headerRight: () => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                        <Ionicons name="settings" size={30} color="white" style={{ marginRight: 10 }} />
+                    </TouchableOpacity>
+                ),
+                tabBarActiveTintColor: projectPalete.color1,
+                tabBarInactiveTintColor: projectPalete.color3,
+                tabBarStyle: { backgroundColor: "#F4ECDD" },
+                tabBarIcon: ({ color, size, focused }) => {
+                    let iconName;
+                    if (route.name === 'Tela inicial') iconName = focused ? 'home' : 'home-outline';
+                    else if (route.name === 'Adicionar agendamento') iconName = focused ? 'add-circle' : 'add-circle-outline';
+                    else if (route.name === 'Agendamentos') iconName = focused ? 'calendar' : 'calendar-outline';
+                    return <Ionicons name={iconName} size={focused ? size + 4 : size} color={color} />;
+                }
+            })}
+        >
+            <Tab.Screen name="Adicionar agendamento" component={AddSchedule} />
+            <Tab.Screen name="Tela inicial" component={Home} />
+            <Tab.Screen name="Agendamentos" component={MySchedules} />
+        </Tab.Navigator>
+    );
+}
