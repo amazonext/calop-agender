39f156f4c58b5917d9ebfaf242b41ea44190d260
Ed <edgarams09@gmail.com>
2025-04-29 03:19:39 -0300

    feat: removi o componente Profile e deixei só o View

--- scripts/src/screens/Settings.jsx
+++ scripts/src/screens/Settings.jsx
@@ -4,17 +4,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // styles
 import styles, { settingsStyles } from '../assets/css/styles';
 
-// components
-import Profile from "../components/Profile";
-
 export default function Settings() {
     const handleResetOnboarding = async () => {
         await AsyncStorage.removeItem('hasSeenOnboarding');
-        // Alert.alert('Reset', 'A tela de introdução será exibida na próxima vez que o app for aberto.');
+        await AsyncStorage.removeItem('hasSeenRegister');
+        console.log('A tela de introdução será exibida na próxima vez que o app for aberto.');
     };
 
     return (
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             <View style={settingsStyles.userBox}>
                 <Image source={ defaultUserPhoto } style={settingsStyles.userPhoto} />
                 <Text style={settingsStyles.Username}>Nome do Usuário</Text>
@@ -23,6 +21,6 @@ export default function Settings() {
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Button title="Resetar Tela de Introdução" onPress={handleResetOnboarding} />
             </View>
-        </SafeAreaView>
+        </View>
     );
 }
