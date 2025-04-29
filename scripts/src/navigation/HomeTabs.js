482e84f2e9e9f78794b12edb551e0089b27cc5ef
Ed <edgarams09@gmail.com>
2025-04-29 03:00:35 -0300

    feat/fix: mudei o arquivo de nome e corrigi os bugs

--- scripts/src/navigation/HomeTabs.js
+++ scripts/src/navigation/HomeTabs.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, Keyboard, TouchableOpacity } from 'react-native';
 
 // Telas
 import Home from '../screens/Home';
@@ -11,9 +11,26 @@ import MySchedules from '../screens/MySchedules';
 // Cores
 import { projectPalete } from '../assets/css/colors';
 
+// navigation
 const Tab = createBottomTabNavigator();
 
-export default function MainTabs({ navigation }) {
+export default function HomeTabs({ navigation }) {
+    const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+            setKeyboardVisible(true);
+        });
+        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+            setKeyboardVisible(false);
+        });
+
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    }, []);
+
     return (
         <Tab.Navigator
             initialRouteName="Tela inicial"
@@ -34,18 +51,20 @@ export default function MainTabs({ navigation }) {
                 ),
                 tabBarActiveTintColor: projectPalete.color1,
                 tabBarInactiveTintColor: projectPalete.color3,
-                tabBarStyle: { backgroundColor: "#F4ECDD" },
+                tabBarStyle: {
+                    backgroundColor: "#F4ECDD",
+                    display: isKeyboardVisible ? 'none' : 'flex'
+                },
                 tabBarIcon: ({ color, size, focused }) => {
                     let iconName;
                     if (route.name === 'Tela inicial') iconName = focused ? 'home' : 'home-outline';
-                    else if (route.name === 'Adicionar modelo') iconName = focused ? 'add-circle' : 'add-circle-outline';
+                    else if (route.name === 'Criar modelo') iconName = focused ? 'add-circle' : 'add-circle-outline';
                     else if (route.name === 'Agendamentos') iconName = focused ? 'calendar' : 'calendar-outline';
                     return <Ionicons name={iconName} size={focused ? size + 4 : size} color={color} />;
                 },
-                tabBarHideOnKeyboard: true
             })}
         >
-            <Tab.Screen name="Adicionar modelo" component={AddSchedule} />
+            <Tab.Screen name="Criar modelo" component={AddSchedule} />
             <Tab.Screen name="Tela inicial" component={Home} />
             <Tab.Screen name="Agendamentos" component={MySchedules} />
         </Tab.Navigator>
