889e6f140899e5907e880445a48ba47e8e062385
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-01 21:15:34 -0300

    feat: apaguei o antigo styles e criei um estilo global

---
+++ scripts/src/assets/css/global/global.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { projectPalete } from '../colors';
+
+export const commonStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: projectPalete.color2,
+        paddingHorizontal: 20,
+    },
+    header: {
+        marginTop: 30,
+        alignItems: 'center',
+    },
+    logo: {
+        width: 120,
+        height: 120,
+        borderRadius: 20,
+        backgroundColor: projectPalete.color6,
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: '600',
+        color: projectPalete.color8,
+        marginTop: 10,
+        marginBottom: 30,
+    },
+    button: {
+        alignItems: 'center',
+        fontSize: 20,
+        padding: 5,
+        backgroundColor: projectPalete.color6,
+        borderRadius: 10,
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 16,
+    }
+});
