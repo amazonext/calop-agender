8210e53f3dcca36cb46380596967cc5b222e1e6b
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-05 11:27:03 -0300

    style: formatei o código de outro jeito

--- scripts/src/components/renderItem.jsx
+++ scripts/src/components/renderItem.jsx
@@ -1,10 +1,25 @@
 import { View, Text, Image } from 'react-native';
 import { renderItemStyles } from '../assets/css/renderItemStyles';
 
-export default RenderItem = ({ item }) => (
-    <View style={{ ...renderItemStyles.slide, backgroundColor: item.backgroundColor }}>
-        <Image style={renderItemStyles.image} source={item.image} />
-        <Text style={{ ...renderItemStyles.titleSlide, color: item.colors.title }}>{item.title}</Text>
-        <Text style={{ ...renderItemStyles.text, color: item.colors.text }}>{item.text}</Text>
-    </View>
-);
+export default function RenderItem({ item }) {
+    return (
+        <View style={{
+            ...renderItemStyles.slide,
+            backgroundColor: item.backgroundColor
+        }}>
+            <Image style={renderItemStyles.image} source={item.image} />
+            <Text style={{
+                ...renderItemStyles.titleSlide,
+                color: item.colors.title
+            }}>
+                {item.title}
+            </Text>
+            <Text style={{
+                ...renderItemStyles.text,
+                color: item.colors.text
+            }}>
+                {item.text}
+            </Text>
+        </View>
+    );
+}
