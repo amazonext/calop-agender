bb9a884e782290b70b020ca23109b91ada10066a
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-05 11:27:42 -0300

    feat: adicionei o navigation ao array para evitar possíveis erros

--- scripts/src/components/Splash.jsx
+++ scripts/src/components/Splash.jsx
@@ -22,7 +22,7 @@ export default function Splash() {
         };
 
         checkFlow();
-    }, []);
+    }, [navigation]);
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
