fce5dbd8dc330eab2d086e7302551489dbb4110a
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-26 04:00:11 -0300

    feat: criei o getter de mensagem

--- scripts/src/utils/messages.js
+++ scripts/src/utils/messages.js
@@ -1,8 +1,9 @@
 import { getFromStorage } from "./storage";
 import { getAppointments } from "./appointments";
 import { drawRandomNumber } from "../helpers/drawRandomNumber";
+import { getCurrentWeekday, getCurrentTurn } from "./date";
 
-function getMessages() {
+function getMessagesFromStorage() {
     const messages = getFromStorage('messages');
 
     return messages;
@@ -12,23 +13,33 @@ function getMessages() {
 async function getConditionalsMessages() {
     const appointments = getAppointments();
     const appointmentsLength = appointments.length;
-    const messagesObject = await getMessages();
+    const messagesObject = await getMessagesFromStorage();
 
     if (appointmentsLength > 10) return messagesObject.conditionals.busyDay[drawRandomNumber(0, 4)];
 }
 
 async function getWeekDayMessage(weekIndex) {
-    const messagesObject = await getMessages();
+    const messagesObject = await getMessagesFromStorage();
     const weekDayMessage = messagesObject.week[weekIndex][drawRandomNumber(0, 4)];
 
     return weekDayMessage;
 }
 
 async function getTurnMessage(currentTurn) {
-    const messagesObject = await getMessages();
+    const messagesObject = await getMessagesFromStorage();
     const turnMessage = messagesObject.turn[currentTurn][drawRandomNumber(0, 4)];
 
     return turnMessage;
 }
 
-export { getConditionalsMessages, getWeekDayMessage, getTurnMessage };
+export function getMessage() {
+    const weekDayIndex = getCurrentWeekday().index;
+    const currentTurn = getCurrentTurn();
+    const messages = [];
+
+    if (!getConditionalsMessages() === undefined) messages.push(getConditionalsMessages());
+
+    messages.push(getWeekDayMessage(weekDayIndex), getTurnMessage(currentTurn));
+
+    return messages[drawRandomNumber(0, messages.length - 1)];
+}
