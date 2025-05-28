9014301c457719543afd3ebb8b4893e34d5f75dc
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-27 21:04:30 -0300

    style: mudei o nome do arquivo, defini um tamanho e removi o texto

---
+++ scripts/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import { View, Text, ActivityIndicator } from 'react-native';
+import { projectPalete } from '../assets/styles/colors';
+
+export default function Loading() {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <ActivityIndicator size="50" color={projectPalete.color1} />
+        </View>
+    );
+}
