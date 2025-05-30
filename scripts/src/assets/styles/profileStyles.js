0b8a5d44a219ae5eedac47c911450398f1e57432
Murylo Costa Santos <murylosantos86@gmail.com>
2025-05-29 23:02:35 -0300

    feat:(Profile): criei o layout e adicionei estilo na tela de perfil.

--- scripts/src/assets/styles/profileStyles.js
+++ scripts/src/assets/styles/profileStyles.js
@@ -1,3 +1,86 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { projectPalete } from "./colors";
+import { global } from "./global/globalStyle";
 
-export const profileStyles = StyleSheet.create({});
+export const profileStyles = StyleSheet.create({
+  ...global, //importei estilos globais
+  
+  
+  container: {
+    flex: 1,
+    backgroundColor: projectPalete.color2,
+    paddingHorizontal: 20,
+  },
+  
+  
+  header: {
+    height: 56,
+    backgroundColor: projectPalete.color2, 
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: projectPalete.color5, 
+    justifyContent: "space-between",
+  },
+  backButton: {
+    width: 24,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: projectPalete.color8,
+  },
+  
+  // Container da foto
+  photoContainer: {
+    marginTop: 30,
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
+  userPhoto: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    backgroundColor: projectPalete.color4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  
+  // Botão de alterar foto
+  changePhotoButton: {
+    marginTop: 10,
+    backgroundColor: projectPalete.color6,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  changePhotoButtonText: {
+    color: projectPalete.color8,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  
+  // Botões de input
+  inputButton: {
+    backgroundColor: projectPalete.color5,
+    marginHorizontal: 16,
+    marginVertical: 10,
+    borderRadius: 12,
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+  },
+  inputButtonLabel: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: projectPalete.color8,
+    marginBottom: 6,
+  },
+  inputButtonValue: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: projectPalete.color3,
+  },
+});
--- scripts/src/screens/Profile.jsx
+++ scripts/src/screens/Profile.jsx
@@ -1,10 +1,45 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+ import { profileStyles as styles } from "../assets/styles/profileStyles";
+import { Ionicons } from '@expo/vector-icons';
 
-// styles
-import { profileStyles } from "../assets/styles/profileStyles";
+export default function Profile({ navigation, user }) {
 
-export default function Profile() {
-    return (
-        <></>
-    );
+  return (
+    <View style={styles.container}>
+
+      {/* Header */}
+      <View style={profileStyles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={profileStyles.backButton}>
+          <Ionicons name="chevron-back" size={24} color="#222" />
+        </TouchableOpacity>
+        <Text style={profileStyles.headerTitle}>Perfil</Text>
+        <View style={{ width: 24 }} /> 
+      </View>
+
+      {/* imagem do Profile */}
+      <View style={profileStyles.photoContainer}>
+        {user?.image_uri ? (
+          <Image source={{ uri: user.image_uri }} style={profileStyles.userPhoto} />
+        ) : (
+          <Ionicons name="person-circle-outline" size={120} color="#888" />
+        )}
+        <TouchableOpacity style={profileStyles.changePhotoButton}>
+          <Text style={profileStyles.changePhotoButtonText}>Alterar imagem de perfil</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* nomes e inputs relacionados*/}
+      <TouchableOpacity style={profileStyles.inputButton}>
+        <Text style={profileStyles.inputButtonLabel}>Nome do usuário</Text>
+        <Text style={profileStyles.inputButtonValue}>{user?.name || "Nome não definido"}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={profileStyles.inputButton}>
+        <Text style={profileStyles.inputButtonLabel}>Nome da empresa</Text>
+        <Text style={profileStyles.inputButtonValue}>{user?.enterprise_name || "Empresa não definida"}</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
 }
