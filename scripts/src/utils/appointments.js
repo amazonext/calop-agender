d2a05e8a1bdbc805be5a995e6bcafb894c677d6e
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2025-05-31 11:28:10 -0300

    feat: adicionei uma nova função

--- scripts/src/utils/appointments.js
+++ scripts/src/utils/appointments.js
@@ -5,18 +5,37 @@ async function getAppointments() {
     return await getItemFromStorage('appointments');
 }
 
-async function addAppointment(data, hour, model) {
-    const appointments = await getAppointments();
+async function addAppointment(date, hour, model) {
+    const appointments = await getAppointments(); // Recupera os agendamentos existentes
+    const [day, month] = date.split('_');
 
     const newAppointment = {
         model,
         hour
     };
 
-    if (!appointments[data]) appointments[data] = [newAppointment];
-    else appointments[data].push(newAppointment);
+    console.log("day: " + day);
+    console.log("month: " + month);
+
+    if (!appointments[month]) appointments[month] = {};
+
+    if (!appointments[month][day]) appointments[month][day] = [];
+
+    appointments[month][day].push(newAppointment);
 
     await saveToStorage('appointments', appointments);
+}
+
+async function getAppointmentsWithDate(date) {
+    const appointments = await getAppointments();
+    const [day, month] = date.split('_');
+
+    // console.log(appointments[month]);
+    // console.log(appointments[month][day]);
+    // return {
+    //     today: appointments[month][day],
+    //     month: appointments[month]
+    // };
 }
 
 function useAppointments() {
@@ -34,4 +53,4 @@ function useAppointments() {
     return appointment;
 }
 
-export { getAppointments, addAppointment, useAppointments };
+export { getAppointments, addAppointment, useAppointments, getAppointmentsWithDate };
